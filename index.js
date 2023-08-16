a51e3c73ed8a5bb70685462f7daaf8d691c643b9
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-16 16:37:23 +0800

    add frontend production build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static("build"))
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method)
