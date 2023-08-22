173b14f7ee5dd6fc307c69dd64253a35ce179f4b
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-22 10:58:30 +0800

    use mongoose

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+    console.log("give password as argument")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.nzj2j49.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// creating new note
+
+// const note = new Note({
+//     content: 'HTML is easy',
+//     important: true
+// })
+
+// note.save().then(result => {
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
+
+// finding notes
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
