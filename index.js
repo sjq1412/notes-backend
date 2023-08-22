6c38b201db6af8c9df575805a4a9e4af11439b15
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-22 11:44:10 +0800

    using database in route handlers

--- index.js
+++ index.js
@@ -19,25 +19,6 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
- 
-
 app.get("/", (request, response) => {
     response.send("<h1>hello world!</h1>")
 })
@@ -45,19 +26,13 @@ app.get("/", (request, response) => {
 app.get("/api/notes", (request, response) => {
   Note.find({}).then(result => {
     response.json(result)
-    mongoose.connection.close()
 })
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-
-  const note = notes.find(n => n.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  }) 
 })
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -68,28 +43,21 @@ app.delete("/api/notes/:id", (request, response) => {
   response.send(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
-  return maxId + 1;
-}
-
 app.post("/api/notes", (request, response) => {
   const body = request.body
 
-  console.log(body)
-
-  if (!body.content) {
-    return response.send(405).end()
+  if (body.content === undefined) {
+    return response.send(400).json({error: "content missing"})
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
     important: body?.important || false,
-    id: generateId()
-  } 
+  })
   
-  console.log(note)
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndpoint = (request, response, next) => {
