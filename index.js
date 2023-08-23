f759f81d5f73cbd9f1cfab46123e4b344a658863
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-23 14:59:12 +0800

    setup and fix eslint

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")  
-const Note = require("./models/note")
+/* eslint-disable no-unused-vars */
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
 
-const app = express();
+const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("build"))
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method)
-  console.log("Path:", request.path)
-  console.log("Body:", request.body)
-  console.log("---")
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
   next()
 }
 
 const errorHandler = (error, request, response, next) => {
   console.log(error)
-  
-  if (error.name === "CastError") {
-    return response.status(400).send({error: "malformatted id"})
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-  if (error.name === "ValidationError") {
-    return response.status(400).send({error: error.message})
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
@@ -32,27 +33,27 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(requestLogger)
 
-app.get("/", (request, response) => {
-    response.send("<h1>hello world!</h1>")
+app.get('/', (request, response) => {
+  response.send('<h1>hello world!</h1>')
 })
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then(result => {
     response.json(result)
-})
+  })
 })
 
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
     if (note) {
       response.json(note)
     } else {
       response.status(404).end()
     }
-  }).catch(error => next(error)) 
+  }).catch(error => next(error))
 })
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
@@ -60,7 +61,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put("/api/notes/:id", (request, response, next) => {
+app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
 
   const note = {
@@ -68,21 +69,21 @@ app.put("/api/notes/:id", (request, response, next) => {
     important: body.important
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, {new: true, runValidators: true, context: "query"})
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
-}) 
+})
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body 
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
     important: body?.important || false,
   })
-  
+
   note.save()
     .then(savedNote => {
       response.json(savedNote)
@@ -92,7 +93,7 @@ app.post("/api/notes", (request, response, next) => {
 
 const unknownEndpoint = (request, response, next) => {
   response.status(404).json({
-    error: "Unknown endpoint"
+    error: 'Unknown endpoint'
   })
 }
 
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("give password as argument")
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,8 +13,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
+  content: String,
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -34,8 +34,8 @@ const Note = mongoose.model('Note', noteSchema)
 // finding notes
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
