6e4aa75bebeb5e7816012697ad3d89ea78ff5d06
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-02 23:06:24 +0800

    add userid to note object when creating a new note

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const result = await Note.find({})
@@ -35,12 +36,18 @@ notesRouter.put('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body?.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
