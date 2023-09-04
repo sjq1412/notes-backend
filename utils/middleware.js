ac8822d7a3ed2ebfdce476de1344b05e5fc27aa7
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-04 21:16:08 +0800

    add token expiration

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,10 +17,13 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
   if (error.name === 'ValidationError') {
-    return response.status(400).send({ error: error.message })
+    return response.status(400).json({ error: error.message })
   }
   if (error.name === 'JsonWebTokenError') {
-    return response.status(401).send({ error: error.message })
+    return response.status(401).json({ error: error.message })
+  }
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
   }
 
   next(error)
