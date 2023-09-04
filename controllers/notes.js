c5f8d6195151f81dc8a20e89035719d1b5a7767e
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-04 21:10:06 +0800

    limiting creating new notes to logged-in users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
@@ -36,15 +37,29 @@ notesRouter.put('/:id', async (request, response) => {
   response.json(updatedNote)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken?.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
     important: body?.important || false,
-    user: user.id
+    user: user._id,
+    date: new Date().toISOString()
   })
 
   const savedNote = await note.save()
--- models/note.js
+++ models/note.js
@@ -11,7 +11,8 @@ const noteSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  }
+  },
+  date: String
 })
 
 noteSchema.set('toJSON', {
